6ea3193fb3f24b2bb012c1f4eab9dda7ddf14b2f
David Michael <devdavem@yahoo.com>
2022-07-14 10:45:39 -0700

    Removed string

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,7 @@ import { TodosModule } from './todos/todos.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    TodosModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://davem2:NK9QCg75rHCBdop7@cluster0.aikdmdw.mongodb.net/?retryWrites=true&w=majority',
-    ),
-  ],
+  imports: [TodosModule],
   controllers: [AppController],
   providers: [AppService],
 })
